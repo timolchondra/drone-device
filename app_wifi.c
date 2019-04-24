8ad912ea3a4f02a9b69ae033544c27af7a40dd3e
timolchondra <tim.olchondra@gmail.com>
2019-04-24 11:08:22 -0700

    only one wifi ssid fix

--- app_wifi.c
+++ app_wifi.c
@@ -23,8 +23,8 @@
 //#define CONFIG_WIFI_SSID      "TimsWifi"
 //#define CONFIG_WIFI_PASSWORD      "beerandwine"
 
-#define CONFIG_WIFI_SSID      "DroneNet"
-#define CONFIG_WIFI_PASSWORD      ""
+//#define CONFIG_WIFI_SSID      "DroneNet"
+//#define CONFIG_WIFI_PASSWORD      ""
 
 //#define CONFIG_WIFI_SSID    "Bebop2-060087"
 //#define CONFIG_WIFI_PASSWORD ""
